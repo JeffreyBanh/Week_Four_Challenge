6e8fd014f758abbf4e24d6e169913ce840d6eed0
JeffreyBanh <banh.jeff@gmail.com>
2021-10-06 23:13:33 -0700

    added more

--- game.js
+++ game.js
@@ -1,5 +1,5 @@
 const question = document.querySelector ('#question');
-const choice = Array,from(document.querySelectorAll('.choice-text'));
+const choice = Array.from(document.querySelectorAll('.choice-text'));
 const progressText = document.querySelector ('#progressText');
 const scoreText = document.querySelector ('#score');
 const progressBarFull = document.querySelector ('#progressBarFull');
@@ -8,15 +8,45 @@ let currentQuestion = {};
 let acceptingAnswers = true;
 let score = 0;
 let questionCOunter = 0;
-let availableQuestion = {};
+let availableQuestion = [];
 
 let questions = [
     {
-        question: "What is 2 + 2?",
-        choice1: '2';
-        choice2: '2';
-        choice3: '2';
-        choice4: '2';
-        answer: 2
+        question: 'What is 2 + 2?',
+        choice1: '2',
+        choice2: '2',
+        choice3: '2',
+        choice4: '2',
+        answer: 2,
+    },
+
+    {
+        question: 'What is 2 + 3?',
+        choice1: '2',
+        choice2: '2',
+        choice3: '2',
+        choice4: '2',
+        answer: 2,
     }
 ]
+
+const SCORE_POINTS = 100;
+const MAX_QUESTIONS = 4;
+
+function startgame(){
+    availableQuestion = [...questions]
+    newQuestions()
+}
+
+function newQuestions(){
+
+    const questionIndex = Math.floor(Math.random() * availableQuestion.length)
+    currentQuestion = availableQuestion[questionIndex]
+    questions.innerHTMl = currentQuestion.questions
+
+    choice.forEach(choice=>{
+        const number = choice.dataset['number']
+        choice.innerHTML = currentQuestion['choice' + number]
+})
+
+startgame()
