cf050c22fd378ab12e6a12eaaede0dfa9a33d866
JeffreyBanh <banh.jeff@gmail.com>
2021-10-11 00:52:23 -0700

    work on functions

--- game.js
+++ game.js
@@ -1,38 +1,110 @@
-const question = document.querySelector ('#question');
-const choice = Array.from(document.querySelectorAll('.choice-text'));
-const progressText = document.querySelector ('#progressText');
+const question = document.querySelector('#question');
+const choices = Array.from(document.querySelectorAll('.choice-text'));
 const scoreText = document.querySelector ('#score');
-const progressBarFull = document.querySelector ('#progressBarFull');
+
 
 let currentQuestion = {};
 let acceptingAnswers = true;
 let score = 0;
-let questionCOunter = 0;
+let questionCounter = 0;
 let availableQuestion = [];
 
 let questions = [
     {
-        question: 'What is 2 + 2?',
-        choice1: '2',
+        question: 'How much of the planet oxygen is produced by the ocean?',
+        choice1: 'less than 50%',
+        choice2: '50% to 60%',
+        choice3: '70% to 80%',
+        choice4: 'over 80%',
+        answer: 3,
+    },
+
+    {
+        question: 'How much does the U.S lies beneath the ocean?',
+        choice1: '1',
         choice2: '2',
-        choice3: '2',
+        choice3: '3',
+        choice4: '5',
+        answer: 4,
+    },
+
+    {
+        question: 'What is 2 + 2?',
+        choice1: '1',
+        choice2: '4',
+        choice3: '3',
         choice4: '2',
         answer: 2,
     },
 
     {
         question: 'What is 2 + 3?',
-        choice1: '2',
+        choice1: '1',
         choice2: '2',
-        choice3: '2',
-        choice4: '2',
-        answer: 2,
+        choice3: '3',
+        choice4: '5',
+        answer: 4,
     }
 ]
 
 const SCORE_POINTS = 100;
 const MAX_QUESTIONS = 4;
 
+function startGame() {
+    score = 0
+    availableQuestions = [...questions]
+
+}
+
+function newQuestions(){
+    if (availableQuestions.length === 0){
+        localStorage.setItem('mostRecentScore', score)
+        return window.location.assign('/highscore.html')
+    }
+
+    const questionIndex = Math.floor(Math.random() * availableQuestions.length)
+    currentQuestion = availableQuestions[questionIndex]
+    question.innerHTML = currentQuestion.question
+
+    choices.forEach(choice =>{
+        const number = choice.dataset['number']
+        choice.innerHTML = currentQuestion['choice' + number]
+    })
+
+    availableQuestions.splice(questionIndex, 1)
+    
+}
+
+startGame()
+newQuestions()
+
+choices.forEach(choice => {
+    choice.addEventListener('click', event => {
+        const selectedChoice = event.target
+        console.log(selectedChoice)
+        const selectedAnswer = selectedChoice.dataset['number']
+        console.log(selectedAnswer)
+
+        let applyClass = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect'
+
+        if (applyClass === 'correct'){
+            incrementalScore(SCORE_POINTS)
+        }
+
+        selectedChoice.parentElement.classList.add(applyClass)
+
+        setTimeout(() => {
+            selectedChoice.parentElement.classList.remove(applyClass)
+            newQuestions()
+        }, 1000)
+    })
+})
+
+function incrementalScore(num){
+    score += num
+    scoreText.innerText = score
+}
+/*
 function startgame(){
     availableQuestion = [...questions]
     newQuestions()
@@ -49,4 +121,5 @@ function newQuestions(){
         choice.innerHTML = currentQuestion['choice' + number]
 })
 
-startgame()
+
+*/
