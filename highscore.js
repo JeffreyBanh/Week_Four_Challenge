cfe7552b41b33e1e28c4bd5ce357bcdf3ac7bc71
JeffreyBanh <banh.jeff@gmail.com>
2021-10-11 01:30:22 -0700

    highscore html

---
+++ highscore.js
@@ -0,0 +1,32 @@
+const username = document.querySelector('#username')
+const saveScore = document.querySelector('#saveScoreBtn')
+const finalScore = document.querySelector('#finalScore')
+const mostRecentScore = document.querySelector('#mostRecentScore')
+
+const highScore = JSON.parse(localStorage.getItem('highScores')) || []
+const MAX_HIGH_SCORE = 5
+
+finalScore.innerText = mostRecentScore
+username.addEventListener('keyup', () =>{
+    saveScoreBtn.disabled = !username.value
+})
+
+saveHighScore = event => {
+    event.preventDefault()
+
+    const score = {
+        score: mostRecentScore,
+        name: username.value
+    }
+
+    highScore.push(score)
+
+    highScore.sort((a,b) => {
+        return b.score - a.scoore
+    })
+
+    highScore.splice(5)
+
+    localStorage.setItem('highScores', JSON.stringify(highScore))
+    window.location.assign('/')
+}
